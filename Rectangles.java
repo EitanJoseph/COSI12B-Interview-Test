a39cb21b03eb91b0710032201667e3721d6887be
Eitan-Joseph <eitan17j@gmail.com>
2019-10-26 11:31:56 -0400

    Adding helper methods to rectangles.java

--- Rectangles.java
+++ Rectangles.java
@@ -4,7 +4,27 @@ import java.awt.Point;
 public class Rectangles {
 
 	public static int getNumRectangles(Point[] points) {
+		return 0;
+	}
+	
+	// Helper methods
+	// You may use either of these two methods in your solution
 
+	/**
+	 * @param x
+	 * @param y
+	 * @return xCy
+	 */
+	public static int choose(int x, int y) {
+		return factorial(x)/(factorial(y)*factorial(x-y));
+	}
+	
+	/**
+	 * @param x
+	 * @return x!
+	 */
+	public static int factorial(int x) {
+		return x <= 1 ? 1 : x * factorial(x - 1);
 	}
 	
 }
